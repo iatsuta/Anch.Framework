ecf1b273d6ed01838b989b29f27cc71da0d14166
iatsuta <98311820+iatsuta@users.noreply.github.com>
2026-07-03 21:15:07 +0200

    upd-refs (#71)

--- src/__RootSolutionItems/RootCommonAssemblyInfo.cs
+++ src/__RootSolutionItems/RootCommonAssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: AssemblyCompany("Anch")]
 [assembly: AssemblyProduct("Anch.Framework")]
 
-[assembly: AssemblyVersion("3.7.2.0")]
+[assembly: AssemblyVersion("3.7.3.0")]
 [assembly: AssemblyInformationalVersion("changes at build")]
 
 #if DEBUG
