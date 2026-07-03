b03c966dab8382d820485ca8bdef6765acd79d8c
iatsuta <terraghost@mail.ru>
2026-07-03 14:10:27 +0200

    clean

--- src/SecuritySystem/Anch.SecuritySystem.Abstractions/ExternalSystem/Management/IElementPrincipalManagementListener.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Anch.SecuritySystem.ExternalSystem.Management;
-
-public interface IElementPrincipalManagementListener<TPrincipal, TPermission, TPermissionRestriction> :
-    IPrincipalManagementListenerBase<
-        PrincipalData<TPrincipal, TPermission, TPermissionRestriction>,
-        PermissionData<TPermission, TPermissionRestriction>>;
