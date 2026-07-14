5a17b0ef038662fb00f28219966acf51c697e2b9
iatsuta <terraghost@mail.ru>
2026-07-14 20:25:59 +0200

    upd

---
+++ src/Testing/Anch.Testing.TUnit/AnchTUnitClassConstructor.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using TUnit.Core.Interfaces;
+
+namespace Anch.Testing.TUnit;
+
+public sealed class AnchTUnitClassConstructor<TEnvironment> :
+    IClassConstructor,
+    ILastTestInTestSessionEventReceiver
+    where TEnvironment : ITestEnvironment, new()
+{
+    private IServiceProviderPool ServiceProviderPool { get; } = AnchTUnitPool<TEnvironment>.Instance;
+
+    public async Task<object> Create(Type type, ClassConstructorMetadata metadata)
+    {
+        var ct = TestSessionContext.Current!.SessionCancellationToken;
+
+        var provider = await this.ServiceProviderPool.GetAsync(ct);
+
+        var beforeHookRan = false;
+
+        try
+        {
+            await provider.RunEnvironmentHooks(EnvironmentHookType.Before, ct);
+            beforeHookRan = true;
+
+            var testInstance = ActivatorUtilities.GetServiceOrCreateInstance(provider, type);
+
+            metadata.TestBuilderContext.Events.OnDispose += async (_, _) =>
+            {
+                try
+                {
+                    await provider.RunEnvironmentHooks(EnvironmentHookType.After, ct);
+                }
+                finally
+                {
+                    await this.ServiceProviderPool.ReleaseAsync(provider, ct);
+                }
+            };
+
+            return testInstance;
+        }
+        catch
+        {
+            if (beforeHookRan)
+            {
+                await provider.RunEnvironmentHooks(EnvironmentHookType.After, ct);
+            }
+
+            await this.ServiceProviderPool.ReleaseAsync(provider, ct);
+            throw;
+        }
+    }
+
+    public ValueTask OnLastTestInTestSession(TestSessionContext context, TestContext testContext) =>
+        this.ServiceProviderPool.DisposeAsync();
+}
---
+++ src/Testing/Anch.Testing.TUnit/AnchTUnitPool.cs
@@ -0,0 +1,7 @@
+﻿namespace Anch.Testing.TUnit;
+
+public static class AnchTUnitPool<TEnvironment>
+    where TEnvironment : ITestEnvironment, new()
+{
+    public static ServiceProviderPool Instance { get; } = new ServiceProviderPool(new TEnvironment(), true);
+}
