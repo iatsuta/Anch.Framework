5ea2848ebe1363681429c498d6db7f4317d97db2
iatsuta <terraghost@mail.ru>
2026-07-14 18:26:47 +0200

    add tunit

---
+++ src/Testing/Anch.Testing.Tunit/AnchFactAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Anch.Testing.Xunit.Engine;
+
+using Xunit;
+using Xunit.v3;
+
+namespace Anch.Testing.Xunit;
+
+[assembly: AnchTUnit<TestEnvironment>]
+
+[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class)]
+public sealed class AnchTUnitAttribute<TEnvironment> : ClassConstructorAttribute
+    where TEnvironment : ITestEnvironment, new()
+{
+    public AnchTUnitAttribute()
+        : base(typeof(AnchTUnitClassConstructor<TEnvironment>))
+    {
+    }
+}
+internal static class AnchTUnitPool<TEnvironment>
+    where TEnvironment : ITestEnvironment, new()
+{
+    public static IServiceProviderPool Instance => new ServiceProviderPool();
+
+    public static async ValueTask DisposeAsync()
+    {
+        if (Interlocked.Exchange(ref _disposed, 1) == 1 || !Pool.IsValueCreated)
+        {
+            return;
+        }
+
+        await Pool.Value.DisposeAsync();
+    }
+}
