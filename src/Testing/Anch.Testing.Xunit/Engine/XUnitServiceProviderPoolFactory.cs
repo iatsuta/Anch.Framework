fca64683de88565a55b17cedf6ab2c8c07f0bfc2
iatsuta <98311820+iatsuta@users.noreply.github.com>
2026-07-14 19:20:12 +0200

    add IServiceProviderPoolFactory (#73)

---
+++ src/Testing/Anch.Testing.Xunit/Engine/XUnitServiceProviderPoolFactory.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+using Xunit;
+
+namespace Anch.Testing.Xunit.Engine;
+
+public class XUnitServiceProviderPoolFactory : IServiceProviderPoolFactory
+{
+    public IServiceProviderPool Create(Assembly assembly, ITestEnvironment testEnvironment)
+    {
+        var collectionBehaviorAttribute = assembly.GetCustomAttribute<CollectionBehaviorAttribute>();
+
+        return new ServiceProviderPool(testEnvironment, !collectionBehaviorAttribute?.DisableTestParallelization);
+    }
+}
