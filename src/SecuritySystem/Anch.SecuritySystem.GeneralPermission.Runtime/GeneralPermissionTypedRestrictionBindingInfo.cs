8890ba79fdd71fd97d75d94ac42be7c1a18326c2
iatsuta <terraghost@mail.ru>
2026-08-11 18:17:21 +0200

    clean

--- src/SecuritySystem/Anch.SecuritySystem.GeneralPermission.Runtime/GeneralPermissionTypedRestrictionBindingInfo.cs
+++ src/SecuritySystem/Anch.SecuritySystem.GeneralPermission.Runtime/GeneralPermissionTypedRestrictionBindingInfo.cs
@@ -12,8 +12,7 @@ public class GeneralPermissionTypedRestrictionBindingInfo<TPermission, TPermissi
     IQueryableSource queryableSource,
     GeneralPermissionRestrictionBindingInfo<TPermissionRestriction, TSecurityContextType, TSecurityContextObjectIdent, TPermission> restrictionBindingInfo,
     IPermissionRestrictionTypeFilterFactory<TPermissionRestriction> permissionRestrictionTypeFilterFactory,
-    IIdentityInfoSource identityInfoSource,
-    IEqualityDomainObjectInfo<TPermission> equalityPermissionInfo) : IPermissionTypedRestrictionBindingInfo<TPermission>
+    IIdentityInfoSource identityInfoSource) : IPermissionTypedRestrictionBindingInfo<TPermission>
     where TPermission : class
     where TPermissionRestriction : class
     where TSecurityContextObjectIdent : notnull
@@ -36,8 +35,7 @@ public class GeneralPermissionTypedRestrictionBindingInfo<TPermission, TPermissi
             return permission => securityContextQ.Where(sc =>
 
                 restrictionQueryable
-                    .Where(restriction => ee.Evaluate(equalityPermissionInfo.EqualityExpression,
-                        ee.Evaluate(restrictionBindingInfo.Permission.Path, restriction), permission))
+                    .Where(restriction => ee.Evaluate(restrictionBindingInfo.Permission.Path, restriction) == permission)
                     .Select(restrictionBindingInfo.SecurityContextObjectId.Path)
                     .Any(securityContextId =>
                         ee.Evaluate(eqIdentExpr, securityContextId, ee.Evaluate(securityContextIdentityInfo.Id.Path, sc)))
@@ -55,8 +53,7 @@ public class GeneralPermissionTypedRestrictionBindingInfo<TPermission, TPermissi
         return ExpressionEvaluateHelper.InlineEvaluate<Func<TPermission, bool>>(ee =>
         {
             return permission => restrictionQueryable
-                .All(restriction => !ee.Evaluate(equalityPermissionInfo.EqualityExpression, ee.Evaluate(restrictionBindingInfo.Permission.Path, restriction),
-                    permission));
+                .All(restriction => ee.Evaluate(restrictionBindingInfo.Permission.Path, restriction) != permission);
         });
     }
 }
