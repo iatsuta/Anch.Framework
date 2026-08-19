59b5bf0f1fd794cf47a37cc2c365d305d5770db9
iatsuta <98311820+iatsuta@users.noreply.github.com>
2026-08-19 22:18:50 +0200

    add-SecurityPathRestriction-hashCode (#86)

--- src/SecuritySystem/Anch.SecuritySystem.Rules/SecurityContextRestriction.cs
+++ src/SecuritySystem/Anch.SecuritySystem.Rules/SecurityContextRestriction.cs
@@ -14,7 +14,27 @@ public record SecurityContextRestriction<TSecurityContext>(
     : SecurityContextRestriction(Required, Key)
     where TSecurityContext : ISecurityContext
 {
+    private int? hashCode;
+
+    protected SecurityContextRestriction(SecurityContextRestriction<TSecurityContext> source)
+        : base(source)
+    {
+        this.Required = source.Required;
+        this.Key = source.Key;
+        this.Filter = source.Filter;
+    }
+
     public override Type SecurityContextType { get; } = typeof(TSecurityContext);
 
     public override SecurityContextRestrictionFilterInfo? RawFilter => this.Filter;
+
+    public virtual bool Equals(SecurityContextRestriction<TSecurityContext>? other) =>
+        object.ReferenceEquals(this, other)
+        || (other is not null
+            && this.GetHashCode() == other.GetHashCode()
+            && this.Required == other.Required
+            && this.Key == other.Key
+            && this.Filter == other.Filter);
+
+    public override int GetHashCode() => this.hashCode ??= HashCode.Combine(this.Required, this.Key, this.Filter);
 }
--- src/__RootSolutionItems/RootCommonAssemblyInfo.cs
+++ src/__RootSolutionItems/RootCommonAssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: AssemblyCompany("Anch")]
 [assembly: AssemblyProduct("Anch.Framework")]
 
-[assembly: AssemblyVersion("3.8.0.0")]
+[assembly: AssemblyVersion("3.8.1.0")]
 [assembly: AssemblyInformationalVersion("changes at build")]
 
 #if DEBUG
