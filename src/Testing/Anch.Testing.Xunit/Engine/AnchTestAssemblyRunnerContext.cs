14ef6b7f0a858e17c81b28e4e702821111046d6e
iatsuta <98311820+iatsuta@users.noreply.github.com>
2026-08-25 14:21:28 +0200

    upd-xunit-to-4.0 (#89)

--- src/Testing/Anch.Testing.Xunit/Engine/AnchTestAssemblyRunnerContext.cs
+++ src/Testing/Anch.Testing.Xunit/Engine/AnchTestAssemblyRunnerContext.cs
@@ -1,4 +1,4 @@
-﻿using Xunit.Sdk;
+using Xunit.Sdk;
 using Xunit.v3;
 
 namespace Anch.Testing.Xunit.Engine;
@@ -12,29 +12,15 @@ public class AnchTestAssemblyRunnerContext(
     AnchTestCollectionRunner commonTestCollectionRunner)
     : XunitTestAssemblyRunnerContext(testAssembly, testCases, executionMessageSink, executionOptions, ct)
 {
-    private static readonly Func<XunitTestAssemblyRunnerBaseContext<IXunitTestAssembly, IXunitTestCase>, SemaphoreSlim?> GetParallelSemaphore =
-        PrivateMemberAccessor.GetInstanceField<XunitTestAssemblyRunnerBaseContext<IXunitTestAssembly, IXunitTestCase>, SemaphoreSlim?>("parallelSemaphore");
-
     public new async ValueTask<RunSummary> RunTestCollection(
         IXunitTestCollection testCollection,
         IReadOnlyCollection<IXunitTestCase> testCases,
         ITestCaseOrderer testCaseOrderer)
     {
-        var parallelSemaphore = GetParallelSemaphore(this);
-
-        if (parallelSemaphore is not null)
-            await parallelSemaphore.WaitAsync(this.CancellationTokenSource.Token);
-
-        try
-        {
-            return await commonTestCollectionRunner.Run(
-                testCollection,
-                testCases, this.ExplicitOption, this.MessageBus,
-                testCaseOrderer, this.Aggregator.Clone(), this.CancellationTokenSource, this.AssemblyFixtureMappings);
-        }
-        finally
-        {
-            parallelSemaphore?.Release();
-        }
+        return await commonTestCollectionRunner.Run(
+            testCollection,
+            testCases, this.ExplicitOption, this.MessageBus,
+            this.Aggregator.Clone(), this.CancellationTokenSource,
+            this.ParallelMode, this.Scheduler, this.AssemblyFixtureMappings);
     }
 }
