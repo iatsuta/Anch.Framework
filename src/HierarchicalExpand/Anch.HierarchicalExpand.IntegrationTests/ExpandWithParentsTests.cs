18b9d4447e316cd5ef110a944733f763d8c086ad
iatsuta <terraghost@mail.ru>
2026-09-10 20:23:38 +0200

    upd

---
+++ src/HierarchicalExpand/Anch.HierarchicalExpand.IntegrationTests/ExpandWithParentsTests.cs
@@ -0,0 +1,48 @@
+﻿using Anch.Core;
+using Anch.Core.ExpressionEvaluate;
+using Anch.GenericQueryable;
+using Anch.GenericRepository;
+using Anch.HierarchicalExpand.IntegrationTests.Domain;
+using Anch.Testing.Xunit;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Anch.HierarchicalExpand.IntegrationTests;
+
+public abstract class ExpandWithParentsTests(IServiceProvider rootServiceProvider)
+{
+    [AnchFact]
+    public async Task ExpandWithParents_IncludesParentOfGrantedEntity(CancellationToken ct)
+    {
+        // Arrange
+        await using var scope = rootServiceProvider.CreateAsyncScope();
+
+        var queryableSource = scope.ServiceProvider.GetRequiredService<IQueryableSource>();
+        var hierarchicalObjectExpanderFactory = scope.ServiceProvider.GetRequiredService<IHierarchicalObjectExpanderFactory>();
+        var hierarchicalObjectExpander = hierarchicalObjectExpanderFactory.Create<Guid>(typeof(BusinessUnit));
+
+        var middleBusinessUnit = await queryableSource.GetQueryable<BusinessUnit>()
+            .Where(businessUnit => businessUnit.Parent != null && businessUnit.Parent.Parent == null)
+            .Select(businessUnit => new { businessUnit.Id, ParentId = businessUnit.Parent!.Id })
+            .GenericFirstAsync(ct);
+
+        var grantedIds = new[] { middleBusinessUnit.Id };
+
+        var expandChildrenExpr = hierarchicalObjectExpander.GetExpandExpression(HierarchicalExpandType.Children);
+
+        var securityFilter = ExpressionEvaluateHelper.InlineEvaluate(ee =>
+            ExpressionHelper.Create((BusinessUnit businessUnit) =>
+                ee.Evaluate(expandChildrenExpr, grantedIds).Contains(businessUnit.Id)));
+
+        var securedIdentsQueryable = queryableSource.GetQueryable<BusinessUnit>()
+            .Where(securityFilter)
+            .Select(businessUnit => businessUnit.Id);
+
+        // Act
+        var result = hierarchicalObjectExpander.ExpandWithParents(securedIdentsQueryable, HierarchicalExpandType.Parents);
+
+        // Assert
+        Assert.Contains(middleBusinessUnit.Id, result.Keys);
+        Assert.Equal(middleBusinessUnit.ParentId, result[middleBusinessUnit.Id]);
+    }
+}
