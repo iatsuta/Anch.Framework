339c35c5bdf4d988eeb361b7b8015bcf55fb8fe3
Atsuta, Ivan <IAtsuta@luxoft.com>
2026-09-11 11:37:27 +0200

    fix DbContextOptionsBuilderExtensions

--- src/GenericQueryable/Anch.GenericQueryable.EntityFramework/DbContextOptionsBuilderExtensions.cs
+++ src/GenericQueryable/Anch.GenericQueryable.EntityFramework/DbContextOptionsBuilderExtensions.cs
@@ -1,13 +1,11 @@
-﻿using Anch.GenericQueryable.DependencyInjection;
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Anch.GenericQueryable.EntityFramework;
 
 public static class DbContextOptionsBuilderExtensions
 {
-    public static DbContextOptionsBuilder UseGenericQueryable(this DbContextOptionsBuilder optionsBuilder, Action<IGenericQueryableSetup>? setupAction = null)
+    public static DbContextOptionsBuilder UseGenericQueryable(this DbContextOptionsBuilder optionsBuilder, Action<IEfGenericQueryableSetup>? setupAction = null)
     {
         var extension = optionsBuilder.Options.FindExtension<GenericQueryableOptionsExtension>()
                         ?? new GenericQueryableOptionsExtension(setupAction);
